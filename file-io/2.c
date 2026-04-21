0331a1564bff2bdc008cd3f1c767c9b16f835b53
aatbip <anantabipal2010@gmail.com>
2026-04-21 22:32:04 +0545

    reading from terminal

--- file-io/2.c
+++ file-io/2.c
@@ -6,12 +6,19 @@
 int main(void) {
   int i_fd = open("./test", O_RDONLY);
   int o_fd = open("./result", O_CREAT | O_RDWR, 0700);
-  char *buf = malloc(32768 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2);
+  char *buf = malloc(2);
   int i = 0;
-  while (read(i_fd, buf, 32768 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2) > 0) {
+  while (read(i_fd, buf, 2) > 0) {
     i++;
-    write(o_fd, buf, 32768 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2);
+    write(o_fd, buf, 2);
   }
   printf("syscalls: %d\n", i);
+  free(buf);
+
+  char input[16];
+  if (read(STDIN_FILENO, input, 16) == -1) {
+    perror("read");
+  }
+  printf("input: %s\n", input);
   return 0;
 }
