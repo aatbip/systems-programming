3a147d1b6674362ad1af88f19bdc24e89cb4c5d3
aatbip <anantabipal2010@gmail.com>
2026-04-21 20:16:52 +0545

    simple form of redirection

--- file-io/1.c
+++ file-io/1.c
@@ -7,11 +7,17 @@
 
 int main(void) {
   mode_t mode = 0700;
-  int o_fd = open("./test", O_CREAT | O_RDWR | O_APPEND, mode);
-  char *buf = "hello world 123\0";
-  if (write(o_fd, buf, strlen(buf)) == -1) {
-    perror("write");
-    exit(EXIT_FAILURE);
+  // simple form of redirection by closing the stdout fd and open call will open the file with
+  // lowest possible fd. Then printf will write to the file instead of writing to the stdout stream.
+  if (close(STDOUT_FILENO) == -1) {
+    perror("close");
   }
+  int o_fd = open("./test", O_CREAT | O_RDWR | O_APPEND, mode);
+  printf("o_fd: %d\n", o_fd);
+  // char *buf = "hello world 123\0";
+  // if (write(o_fd, buf, strlen(buf)) == -1) {
+  //   perror("write");
+  //   exit(EXIT_FAILURE);
+  // }
   exit(EXIT_SUCCESS);
 }
