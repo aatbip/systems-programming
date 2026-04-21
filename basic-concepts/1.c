f457d83f21207aa2ab39cfe57eb40a149cf6356e
aatbip <anantabipal2010@gmail.com>
2026-04-21 10:47:59 +0545

    errno and perror

--- basic-concepts/1.c
+++ basic-concepts/1.c
@@ -1,7 +1,12 @@
+#include <asm-generic/errno-base.h>
+#include <asm-generic/errno.h>
+#include <errno.h>
 #include <gnu/libc-version.h>
 #include <stdio.h>
+
 int main(void) {
   printf("%s\n", gnu_get_libc_version());
-
+  errno = EINPROGRESS; // to demo perror
+  perror("demo");
   return 0;
 }
