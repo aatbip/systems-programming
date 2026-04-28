388c4ff832dd389336e0d6fa939904adf2a11028
aatbip <anantabipal2010@gmail.com>
2026-04-28 22:12:41 +0545

    cmd line arguments

--- processes/1.c
+++ processes/1.c
@@ -1,7 +1,14 @@
+#define _GNU_SOURCE
+
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void) {
+int main(int argc, char **argv) {
+  printf("%d\n", getpid());
+  printf("%s\n", program_invocation_name);
+  printf("%s\n", program_invocation_short_name);
   printf("%ld\n", sysconf(_SC_PAGE_SIZE));
+  sleep(15);
   return 0;
 }
