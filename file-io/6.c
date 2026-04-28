18ea7c6c5631d40c69c25039b7274c68208f0f45
aatbip <anantabipal2010@gmail.com>
2026-04-28 13:52:00 +0545

    Testing /dev/fd

--- file-io/6.c
+++ file-io/6.c
@@ -1,13 +1,19 @@
-#define _LARGEFILE64_SOURCE
-// for large file access
 #include <fcntl.h>
 #include <unistd.h>
 
 #include <stdio.h>
 
 int main(void) {
-  int fd = open64("test.txt", O_CREAT | O_RDWR, 0700);
-  lseek64(fd, 10111222333, SEEK_SET);
-  write(fd, "test", 4);
+  // int fd = open64("test.txt", O_CREAT | O_RDWR, 0700);
+  // lseek64(fd, 10111222333, SEEK_SET);
+  // write(fd, "test", 4);
+
+  printf("PID: %d\n", getpid());
+  sleep(5);
+  int fd = open("test", O_CREAT | O_RDONLY, 0700);
+  printf("fd: %d\n", fd);
+  sleep(5);
+  close(fd);
+  sleep(5);
   return 0;
 }
