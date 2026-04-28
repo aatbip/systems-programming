a17fc0c68f20040a53f10028c6d778550a2f9634
aatbip <anantabipal2010@gmail.com>
2026-04-28 10:41:30 +0545

    Large fille apis and creating a large file with hole

--- file-io/6.c
+++ file-io/6.c
@@ -1,6 +1,13 @@
-// for large file access
 #define _LARGEFILE64_SOURCE
+// for large file access
+#include <fcntl.h>
+#include <unistd.h>
 
 #include <stdio.h>
 
-int main(void) { off64_t a; }
+int main(void) {
+  int fd = open64("test.txt", O_CREAT | O_RDWR, 0700);
+  lseek64(fd, 10111222333, SEEK_SET);
+  write(fd, "test", 4);
+  return 0;
+}
