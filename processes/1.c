78755b503a21903256f514b5f725a69064c8828f
aatbip <anantabipal2010@gmail.com>
2026-04-28 18:02:40 +0545

    know the page size of vm

--- processes/1.c
+++ processes/1.c
@@ -1,14 +1,7 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
 
 int main(void) {
-  printf("[%d] process running\n", getpid());
-  char buf[4];
-  for (int i = 0; i < 1000; i++) {
-    int len = snprintf(buf, sizeof(buf), "%d ", i);
-    write(STDOUT_FILENO, buf, len);
-    sleep(1);
-  }
+  printf("%ld\n", sysconf(_SC_PAGE_SIZE));
   return 0;
 }
