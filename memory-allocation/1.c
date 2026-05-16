1cd28b4c44d666d91e961f391284460e782dba0f
aatbip <anantabipal2010@gmail.com>
2026-05-16 22:15:28 +0545

    132kb bulk allocation by malloc demo

---
+++ memory-allocation/1.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+  void *a = sbrk(0);
+  printf("a: %p\n", a);
+
+  char *p = malloc(1024);
+  void *b = sbrk(0);
+  printf("b: %p\n", b);
+  printf("diff: %ld kb\n", (b - a) / 1024);
+
+  char *q = malloc(1024 * 66);
+  char *r = malloc(1024 * 66);
+  void *c = sbrk(0);
+  printf("c: %p\n", c);
+  printf("diff: %ld kb\n", (c - b) / 1024);
+
+  // void *d = sbrk(0);
+  // printf("d: %p\n", d);
+  // printf("diff: %ld kb\n", (d - c) / 1024);
+
+  return 0;
+}
