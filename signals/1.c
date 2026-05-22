6d25e2c059cdf7ac09768259e50c93ca3c179077
aatbip <anantabipal2010@gmail.com>
2026-05-22 18:24:19 +0545

    changing the sigsev default handler

--- signals/1.c
+++ signals/1.c
@@ -8,6 +8,7 @@ void (*oldHandler)(int);
 void newHandler(int sig) {
   //
   printf("\nhey %d\n", sig);
+  exit(EXIT_FAILURE);
 };
 
 typedef void (*hd_t)(int);
@@ -21,7 +22,8 @@ hd_t test(int n, hd_t new_hd) {
 }
 
 int main(void) {
-  oldHandler = signal(SIGINT, newHandler);
+  printf("pid: %d\n", getpid());
+  oldHandler = signal(SIGSEGV, newHandler);
   sig_t s; // sighandler_t type alias
 
   if (oldHandler == SIG_ERR) {
@@ -29,15 +31,18 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  sleep(2);
+  // sleep(2);
   printf("do something...\n");
 
-  if (signal(SIGINT, SIG_IGN) == SIG_ERR) {
-    perror("signal 1");
-    exit(EXIT_FAILURE);
-  }
-  while (1)
-    ;
+  int *a;
+  a[1] = 100;
+
+  // if (signal(SIGINT, SIG_DFL) == SIG_ERR) {
+  //   perror("signal 1");
+  //   exit(EXIT_FAILURE);
+  // }
+  // while (1)
+  //   ;
 
   exit(EXIT_SUCCESS);
 }
