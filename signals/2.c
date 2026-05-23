852421e5448650cdb2eb6bea1916d02a57831a70
aatbip <anantabipal2010@gmail.com>
2026-05-23 17:17:57 +0545

    Send signal to child process from parent

--- signals/2.c
+++ signals/2.c
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void handler(int sig) {
+  if (sig == SIGTERM) {
+    printf("SIGTERM invoked\n");
+  }
+
+  if (sig == SIGQUIT) {
+    printf("Quiting normally...\n");
+    exit(EXIT_SUCCESS);
+  }
+}
+
 int main(int argc, char **argv) {
   if ((kill(atoi(argv[1]), atoi(argv[2]))) == -1) {
     perror("kill");
@@ -13,9 +24,15 @@ int main(int argc, char **argv) {
   pid_t pid;
   // new process
   if ((pid = fork() == 0)) {
+    signal(SIGTERM, handler);
   }
 
   // old process
   if (pid > 0) {
+
+    for (;;)
+      if (kill(pid, atoi(argv[1])) == -1) {
+        perror("kill");
+      }
   }
 }
