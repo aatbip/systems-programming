edbf2eaa68a8b8c32fbc89d049cd68a9ad8aacd6
aatbip <anantabipal2010@gmail.com>
2026-05-23 17:13:33 +0545

    Register multiple signals in the handler

--- signals/2.c
+++ signals/2.c
@@ -1,6 +1,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
   if ((kill(atoi(argv[1]), atoi(argv[2]))) == -1) {
@@ -8,4 +9,13 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
   exit(EXIT_SUCCESS);
+
+  pid_t pid;
+  // new process
+  if ((pid = fork() == 0)) {
+  }
+
+  // old process
+  if (pid > 0) {
+  }
 }
