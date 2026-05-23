055a52b685e083cbe0d67a0c4522ec632a5793dc
aatbip <anantabipal2010@gmail.com>
2026-05-23 17:39:05 +0545

    Replace with scanf

--- signals/2.c
+++ signals/2.c
@@ -15,25 +15,27 @@ void handler(int sig) {
 }
 
 int main(int argc, char **argv) {
-  if ((kill(atoi(argv[1]), atoi(argv[2]))) == -1) {
-    perror("kill");
-    exit(EXIT_FAILURE);
-  }
-  exit(EXIT_SUCCESS);
 
   pid_t pid;
   // new process
-  if ((pid = fork() == 0)) {
+  if ((pid = fork()) == 0) {
+    printf("Child running... PID %d\n", getpid());
     signal(SIGTERM, handler);
     signal(SIGQUIT, handler);
   }
 
-  // old process
+  // parent process
   if (pid > 0) {
+    printf("Parent PID: %d\n", getpid());
     signal(SIGQUIT, handler);
-    for (;;)
-      if (kill(pid, atoi(argv[1])) == -1) {
+    for (;;) {
+      int s;
+      scanf("%d", &s);
+      printf("reading %d\n", s);
+      if (kill(pid, s) == -1) {
         perror("kill");
+        exit(EXIT_FAILURE);
       }
+    }
   }
 }
