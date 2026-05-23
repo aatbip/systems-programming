e8961a05532836c38bd3386dafa479fea9388f93
aatbip <anantabipal2010@gmail.com>
2026-05-24 00:29:09 +0545

    Check bit set using bitwise operations

--- signals/3.c
+++ signals/3.c
@@ -9,16 +9,23 @@ int main(int argc, char *argv[]) {
   printf("%s\n", s);
 
   sigset_t set;
+  printf("2nd bit set? %d\n", (((set.__val[0] >> 2) & 1) == 1));
+  printf("14th bit set? %d\n", (((set.__val[0] >> 14) & 1) == 1));
+  printf("0st bit set? %d\n", (((set.__val[0] >> 0) & 1) == 1));
   sigaddset(&set, 3);
   sigaddset(&set, 15);
+  sigaddset(&set, 1);
+  printf("2nd bit set? %d\n", (((set.__val[0] >> 2) & 1) == 1));
+  printf("14th bit set? %d\n", (((set.__val[0] >> 14) & 1) == 1));
+  printf("0st bit set? %d\n", (((set.__val[0] >> 0) & 1) == 1));
 
-  printf("%d\n", sigismember(&set, atoi(argv[1])));
-
-  for (int i = 1; i < NSIG; i++) {
-    if (sigismember(&set, i)) {
-      printf("%d %s\n", i, strsignal(i));
-    }
-  }
-
+  // printf("%d\n", sigismember(&set, atoi(argv[1])));
+  //
+  // for (int i = 1; i < NSIG; i++) {
+  //   if (sigismember(&set, i)) {
+  //     printf("%d %s\n", i, strsignal(i));
+  //   }
+  // }
+  //
   exit(EXIT_SUCCESS);
 }
