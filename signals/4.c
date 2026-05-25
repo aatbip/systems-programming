ef6a9f71e93f356cc5ba78b0429c822dd6f8d0a8
aatbip <anantabipal2010@gmail.com>
2026-05-25 22:24:41 +0545

    Signal handler function definition

--- signals/4.c
+++ signals/4.c
@@ -1,8 +1,21 @@
 /*Analyzing what may possibly happen when sending multiple signals to a process concurrently.*/
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+void sig_handle(int sig) {
+  if (sig == SIGINT) {
+    printf("%s triggered.", strsignal(sig));
+  }
+  if (sig == SIGTERM) {
+    printf("%s triggered.", strsignal(sig));
+    exit(EXIT_SUCCESS);
+  }
+}
+
 int main(int argc, char *argv[]) {
   int proc;
   switch ((proc = fork())) {
