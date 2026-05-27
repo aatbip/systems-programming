1b7a07435463d4f688cd1e2e09dfab2f8d75f943
aatbip <anantabipal2010@gmail.com>
2026-05-27 10:04:32 +0545

    Check for existing signal mask

---
+++ signals/5.c
@@ -0,0 +1,19 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  sigset_t oldset;
+  if (sigprocmask(0, NULL, &oldset) == -1) {
+    perror("sigprocmask");
+  }
+
+  for (int i = 0; i < NSIG; i++) {
+    if (sigismember(&oldset, i)) {
+      printf("%s\n", strsignal(i));
+    }
+  }
+
+  exit(EXIT_SUCCESS);
+}
