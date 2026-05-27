d50581b67452a0bb36e42eef918a23affc2a5a0b
aatbip <anantabipal2010@gmail.com>
2026-05-27 10:10:16 +0545

    Add to sig mask

--- signals/5.c
+++ signals/5.c
@@ -6,7 +6,7 @@
 void print_sig(sigset_t *set) {
   for (int i = 1; i < NSIG; i++) {
     if (sigismember(set, i)) {
-      printf("%s\n", strsignal(i));
+      printf("sig %d %s\n", i, strsignal(i));
     }
   }
 }
@@ -16,6 +16,17 @@ int main(void) {
   if (sigprocmask(0, NULL, &oldset) == -1) {
     perror("sigprocmask");
   }
+
+  sigaddset(&newset, 1);
+  sigaddset(&newset, 2);
+  sigaddset(&newset, 3);
+  sigaddset(&newset, 4);
+
+  if (sigprocmask(SIG_BLOCK, &newset, &oldset) == -1) {
+    perror("sigprocmask");
+  }
+
+  print_sig(&newset);
 
   exit(EXIT_SUCCESS);
 }
