4ea7c73e6939e83890539dcbe4065afb31aa29f4
aatbip <anantabipal2010@gmail.com>
2026-05-27 18:08:07 +0545

    Test what happens when multiple signals are invoked while handler is running

--- signals/6.c
+++ signals/6.c
@@ -1,1 +1,32 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
 
+void handler(int sig) {
+  struct timeval tv;
+  struct timezone tz;
+  int i = gettimeofday(&tv, &tz);
+  printf("fired\n");
+  sleep(5);
+  printf("time %ld\n", tv.tv_sec);
+}
+
+int main(void) {
+  printf("pid: %d\n", getpid());
+  struct sigaction act;
+  act.sa_handler = handler;
+  // act.sa_flags = SA_NODEFER;
+  if (sigaction(SIGTERM, &act, NULL) == -1) {
+    perror("sigaction");
+  }
+
+  // if (signal(SIGTERM, handler) == SIG_ERR) {
+  //   perror("signal");
+  // }
+
+  for (;;)
+    pause();
+  exit(EXIT_SUCCESS);
+}
