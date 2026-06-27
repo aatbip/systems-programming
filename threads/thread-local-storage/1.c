b5c048a3195f4779c399fd5ee689a6ed95cd6774
aatbip <anantabipal2010@gmail.com>
2026-06-27 23:07:53 +0545

    strerror1 function that simulates the buffer override when called by multiple threads

---
+++ threads/thread-local-storage/1.c
@@ -0,0 +1,42 @@
+#define _GNU_SOURCE
+#include <asm-generic/errno-base.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_ERROR_LEN 256
+
+static char buf[MAX_ERROR_LEN]; /* Statically allocated return buffer */
+
+char *strerror1(int err) {
+  if (err < 0) {
+    snprintf(buf, MAX_ERROR_LEN, "Unknown error %d", err);
+  } else {
+    strncpy(buf, strerror(err), MAX_ERROR_LEN - 1);
+    buf[MAX_ERROR_LEN - 1] = '\0';
+  }
+  return buf;
+}
+
+void *func(void *p) {
+  char *s;
+  printf("strerr will be called from func\n");
+  s = strerror1(EPERM);
+  printf("from func: %s\n", s);
+  return NULL;
+}
+
+int main(void) {
+  char *s;
+  s = strerror1(EINVAL);
+  printf("strerr called from main\n");
+
+  pthread_t th;
+  pthread_create(&th, NULL, func, NULL);
+  pthread_join(th, NULL);
+
+  printf("from main: %s\n", s);
+
+  return 0;
+}
