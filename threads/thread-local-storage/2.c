2dd80e83ebeff407c93b71f67477662dd20a2177
aatbip <anantabipal2010@gmail.com>
2026-06-27 23:57:53 +0545

    __thread keyword to create thread local storage

---
+++ threads/thread-local-storage/2.c
@@ -0,0 +1,44 @@
+/*This program shows how to allocate thread specific data.*/
+
+#define _GNU_SOURCE
+#include <asm-generic/errno-base.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_ERROR_LEN 256
+
+char *strerror1(int err) {
+  static __thread char buf[MAX_ERROR_LEN];
+
+  if (err < 0) {
+    snprintf(buf, MAX_ERROR_LEN, "Unknown error %d", err);
+  } else {
+    strncpy(buf, strerror(err), MAX_ERROR_LEN - 1);
+    buf[MAX_ERROR_LEN - 1] = '\0';
+  }
+  return buf;
+}
+
+void *func(void *p) {
+  char *s;
+  printf("strerr will be called from func\n");
+  s = strerror1(EPERM);
+  printf("from func: %s\n", s);
+  return NULL;
+}
+
+int main(void) {
+  char *s;
+  s = strerror1(EINVAL);
+  printf("strerr called from main\n");
+
+  pthread_t th;
+  pthread_create(&th, NULL, func, NULL);
+  pthread_join(th, NULL);
+
+  printf("from main: %s\n", s);
+
+  return 0;
+}
