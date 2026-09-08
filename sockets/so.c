46ae03f6a6cd8e3f4e08d9c21760b4cefe508a38
aatbip <anantabipal2010@gmail.com>
2026-09-08 16:41:02 +0545

    bind and addr

--- sockets/so.c
+++ sockets/so.c
@@ -1,9 +1,16 @@
+#include <bits/sockaddr.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 
 int main(void) {
-  int s = socket(AF_UNIX, SOCK_STREAM, 0);
-  printf("%d\n", s);
+  int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+  printf("%d\n", fd);
+
+  struct sockaddr *addr = malloc(sizeof(struct sockaddr));
+  addr->sa_family = AF_INET;
+
+  int c = bind(fd, addr, sizeof(*addr));
 
   return 0;
 }
