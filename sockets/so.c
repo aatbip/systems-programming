19bdafba2e773d8b75c06d3efca14ca056f75617
aatbip <anantabipal2010@gmail.com>
2026-09-10 10:39:59 +0545

    Create unix socket and bind it to an address

--- sockets/so.c
+++ sockets/so.c
@@ -1,16 +1,30 @@
-#include <bits/sockaddr.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 
 int main(void) {
+  const char *PATH = "/tmp/sockunix";
+
+  struct sockaddr_un addr;
+  memset(&addr, 0, sizeof(struct sockaddr_un));
+
+  addr.sun_family = AF_UNIX;
+  strcpy(addr.sun_path, PATH);
+  addr.sun_path[strlen(PATH) + 1] = '\0';
+
   int fd = socket(AF_UNIX, SOCK_STREAM, 0);
-  printf("%d\n", fd);
+  if (fd != 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
 
-  struct sockaddr *addr = malloc(sizeof(struct sockaddr));
-  addr->sa_family = AF_INET;
-
-  int c = bind(fd, addr, sizeof(*addr));
+  if (bind(fd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) != 0) {
+    perror("bind");
+    exit(EXIT_FAILURE);
+  }
 
   return 0;
 }
