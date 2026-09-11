583fd39eb17e0143f3a0b2dacb651fca7239c037
aatbip <anantabipal2010@gmail.com>
2026-09-11 14:33:56 +0545

    simple dgram server

---
+++ sockets/un_datagram_server.c
@@ -0,0 +1,47 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define PATH "/tmp/dgram_server"
+#define BUF_SIZE 1024
+
+int main(void) {
+  int fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+  if (fd == -1) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_un s_addr;
+  memset(&s_addr, 0, sizeof(struct sockaddr_un));
+  s_addr.sun_family = AF_UNIX;
+  strncpy(s_addr.sun_path, PATH, sizeof(s_addr.sun_family) - 1);
+
+  if (bind(fd, (struct sockaddr *)&s_addr, sizeof(struct sockaddr_un)) == -1) {
+    perror("bind");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_un c_addr;
+  int len = sizeof(struct sockaddr_un);
+  char buf[BUF_SIZE];
+  for (;;) {
+    int recvbyte = recvfrom(fd, buf, BUF_SIZE, 0, (struct sockaddr *)&c_addr, (socklen_t *)&len);
+
+    if (recvbyte == -1) {
+      perror("recvfrom");
+      exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < strlen(buf); i++) {
+      buf[i] = toupper(buf[i]);
+    }
+
+    if (sendto(fd, buf, BUF_SIZE, 0, (struct sockaddr *)&c_addr, (socklen_t)len) < recvbyte) {
+      printf("Failed to send from server\n");
+    }
+  }
+}
